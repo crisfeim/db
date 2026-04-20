7d0ebc9ce9fd4fbbeab0d10b66300930b337e527
Cristian Rojas <cristian.rojas@live.fr>
2026-04-20 02:53:00 +0200

    scheduled 02:53 push

--- cron.sh
+++ cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+NOW=$(date +"%H:%M")
+cd /Users/cristian/db || exit
 
-cd ~/db || exit
-
-git add db.db
+git add .
 
 if ! git diff-index --quiet HEAD; then
-    git commit -m "scheduled 21:00 push"
-    git push origin main
+    git commit -m "scheduled $NOW push"
 fi
+
+GIT_SSH_COMMAND="ssh -i /Users/cristian/.ssh/id_ed25519 -o IdentitiesOnly=yes -o StrictHostKeyChecking=no" git push origin main
